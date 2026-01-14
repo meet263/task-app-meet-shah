9c1dd23548952e36da29e4f732174dee0713850f
Meet Shah <meet.learnvern@gmail.com>
2026-01-14 17:07:42 +0530

    Initialise vue file structure and routing

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
 import './bootstrap';
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App.vue';
+
+const routes = [
+    //
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
---
+++ resources/js/app.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="container mt-5">
+        <h1 class="mb-4">Task App - Meet Shah</h1>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App'
+};
+</script>
